67dbb5b4dacad0d29a6b44c1a5679223a8edf287
Kai Shoya <shonari77@gmail.com>
2023-04-21 01:30:25 +0900

    Aboutページ追加 See #14

---
+++ pages/about/index.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <table class="table is-hoverable is-fullwidth is-striped">
+    <tbody>
+      <tr>
+        <th colspan="2">
+          <NuxtLink
+            to="/"
+            class="display is-block"
+          >
+            ソースコード
+          </NuxtLink>
+        </th>
+      </tr>
+      <tr>
+        <th colspan="2">
+          <NuxtLink
+            to="/"
+            class="display is-block"
+          >
+            利用規約
+          </NuxtLink>
+        </th>
+      </tr>
+      <tr>
+        <th>ライセンス</th>
+        <td>
+          <NuxtLink
+            to="https://github.com/KaiShoya/drink-counter/blob/main/LICENSE.md"
+            class="display is-block"
+          >
+            MIT
+          </NuxtLink>
+        </td>
+      </tr>
+
+      <tr>
+        <th>バージョン</th>
+        <td>{{ $config.public.version }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+a {
+  color: hsl(0, 0%, 21%);
+}
+</style>
