6534e441c595f0e1495993e2439012838163a02d
Kai Shoya <shonari77@gmail.com>
2023-09-17 17:38:21 +0900

    DrinkCountersのデータを整形する機能を共通化した（monthly, totalで利用するため）

--- composables/common.ts
+++ composables/common.ts
@@ -1,3 +1,5 @@
+import { DrinkCounter } from '~/store/data/types/drinkCounter'
+
 export const useProcessDate = () => {
   /**
    * DateオブジェクトをYYYY-MM形式に変換する
@@ -5,7 +7,30 @@ export const useProcessDate = () => {
    * @returns string YYYY-MM
    */
   const processIntoYearMonth = (date: Date) => `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}`
+
+  /**
+   * カレンダー、棒グラフで利用できる形にデータを整形する
+   * @param drinkCounters
+   * @param getDrinksIdArray
+   * @returns
+   */
+  const formatDrinkCounters = (drinkCounters: Array<DrinkCounter>, getDrinksIdArray: Array<number>) => {
+    const data: { [key: string]: Array<number> } = {}
+
+    drinkCounters.forEach((drinkCounter) => {
+      if (!Object.getOwnPropertyDescriptor(data, drinkCounter.date)) {
+        // 配列初期化
+        data[drinkCounter.date] = new Array(getDrinksIdArray.length + 1)
+        data[drinkCounter.date].fill(0)
+      }
+      data[drinkCounter.date][getDrinksIdArray.indexOf(drinkCounter.drink_id) + 1] = drinkCounter.count
+      data[drinkCounter.date][0] += drinkCounter.count
+    })
+    return data
+  }
+
   return {
     processIntoYearMonth,
+    formatDrinkCounters,
   }
 }
--- store/pages/data/monthly.ts
+++ store/pages/data/monthly.ts
@@ -7,7 +7,7 @@ import { useDrinksStore } from '~/store/data/drinks'
 export const useMonthlyStore = defineStore('monthlyStore', () => {
   // const { $i18n } = useNuxtApp()
   const { supabase } = useSupabaseStore()
-  const { processIntoYearMonth } = useProcessDate()
+  const { processIntoYearMonth, formatDrinkCounters } = useProcessDate()
   const drinkCountersStore = useDrinkCountersStore()
   const { drinkCounters } = storeToRefs(drinkCountersStore)
   const { fetchDrinkCountersPerMonth } = drinkCountersStore
@@ -27,12 +27,6 @@ export const useMonthlyStore = defineStore('monthlyStore', () => {
     return { year, month }
   })
 
-  const fetchSumCountPerMonth = async () => {
-    const { year, month } = computedYearMonth.value
-    const { data } = await supabase.rpc('sum_count_per_month', { year, month })
-    sumCountPerMonth.value = data ?? []
-  }
-
   const fetchDrinkCounters = async () => {
     const { year, month } = computedYearMonth.value
     await fetchDrinks()
@@ -41,29 +35,17 @@ export const useMonthlyStore = defineStore('monthlyStore', () => {
     graphDataTitle.value = [...graphDataTitleBase, ...getDrinksNameArray.value]
   }
 
-  /**
-   * カレンダー、棒グラフで利用するデータの基礎
-   */
-  const computeData = computed(() => {
-    const data: { [key: string]: Array<number> } = {}
-
-    drinkCounters.value.forEach((drinkCounter) => {
-      if (!Object.getOwnPropertyDescriptor(data, drinkCounter.date)) {
-        // 配列初期化
-        data[drinkCounter.date] = new Array(getDrinksIdArray.value.length + 1)
-        data[drinkCounter.date].fill(0)
-      }
-      data[drinkCounter.date][getDrinksIdArray.value.indexOf(drinkCounter.drink_id) + 1] = drinkCounter.count
-      data[drinkCounter.date][0] += drinkCounter.count
-    })
-    return data
-  })
+  const fetchSumCountPerMonth = async () => {
+    const { year, month } = computedYearMonth.value
+    const { data } = await supabase.rpc('sum_count_per_month', { year, month })
+    sumCountPerMonth.value = data ?? []
+  }
 
   /**
    * カレンダー用データ
    */
   const computeCalendarData = computed(() => {
-    return Object.entries(computeData.value).map(([key, value]) => {
+    return Object.entries(formatDrinkCounters(drinkCounters.value, getDrinksIdArray.value)).map(([key, value]) => {
       return {
         date: key,
         count: value[0],
@@ -77,7 +59,7 @@ export const useMonthlyStore = defineStore('monthlyStore', () => {
   const computeGraphData = computed(() => {
     return [
       graphDataTitle.value,
-      ...Object.entries(computeData.value).map(([key, value]) => [key as string | Number].concat(value)),
+      ...Object.entries(formatDrinkCounters(drinkCounters.value, getDrinksIdArray.value)).map(([key, value]) => [key as string | Number].concat(value)),
     ]
   })
 
