4e2a7611eeb4c130dd927de978eba7d9a9839fc4
Kai Shoya <shonari77@gmail.com>
2023-11-06 03:22:11 +0900

    supabase db diffコマンドでエラーが発生するため、DROP FUNCITONを追加した （SQLエディターでは動く + コマンドで自動生成したファイルのはず。。。）

--- supabase/migrations/20231103134959_upgrade_functions.sql
+++ supabase/migrations/20231103134959_upgrade_functions.sql
@@ -1,5 +1,6 @@
 set check_function_bodies = off;
 
+DROP FUNCTION public.decrement;
 CREATE OR REPLACE FUNCTION public.decrement(row_id bigint)
  RETURNS integer
  LANGUAGE sql
@@ -9,6 +10,7 @@ where id = row_id and "count" > 0;
 select "count" from drink_counters where id = row_id;$function$
 ;
 
+DROP FUNCTION public.increment;
 CREATE OR REPLACE FUNCTION public.increment(row_id bigint)
  RETURNS integer
  LANGUAGE sql
