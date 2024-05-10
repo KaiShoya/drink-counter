7b01b8d0486a668161e6296e521ee6ebec553ec1
Kai Shoya <shonari77@gmail.com>
2024-05-10 21:35:04 +0900

    feat(index): Todayボタンを追加した

--- components/organisms/picker/DatePicker.vue
+++ components/organisms/picker/DatePicker.vue
@@ -7,12 +7,20 @@ import { useIndexStore } from '@/store/pages/index'
 
 const indexStore = useIndexStore()
 const { date } = storeToRefs(indexStore)
-const { prevDate, nextDate } = indexStore
+const { fetchDate, prevDate, nextDate } = indexStore
 </script>
 
 <template>
   <div class="columns is-mobile my-4 mx-0">
     <MoleculesMinusButton :click-function="prevDate" />
+    <button
+      class="column is-50px button is-large ml-2"
+      @click="fetchDate"
+    >
+      <span class="icon is-medium">
+        <i class="mdi mdi-calendar-today" />
+      </span>
+    </button>
     <input
       v-model="date"
       class="column input is-large mx-2"
@@ -21,3 +29,10 @@ const { prevDate, nextDate } = indexStore
     <MoleculesPlusButton :click-function="nextDate" />
   </div>
 </template>
+
+<style scoped>
+.is-50px {
+  flex: none;
+  width: 50px;
+}
+</style>
