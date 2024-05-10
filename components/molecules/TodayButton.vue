0bb04ec120618663291c55137c7ae8a2c1cb2ff5
Kai Shoya <shonari77@gmail.com>
2024-05-10 22:00:10 +0900

    feat: Todayボタンをコンポーネント化した

---
+++ components/molecules/TodayButton.vue
@@ -0,0 +1,25 @@
+<script
+  setup
+  lang="ts"
+>
+defineProps<{
+  clickFunction: Function,
+}>()
+</script>
+<template>
+  <button
+    class="column is-50px button is-large ml-2"
+    @click="clickFunction"
+  >
+    <span class="icon is-medium">
+      <i class="mdi mdi-calendar-today" />
+    </span>
+  </button>
+</template>
+
+<style scoped>
+.is-50px {
+  flex: none;
+  width: 50px;
+}
+</style>
--- components/organisms/picker/DatePicker.vue
+++ components/organisms/picker/DatePicker.vue
@@ -13,14 +13,7 @@ const { fetchDate, prevDate, nextDate } = indexStore
 <template>
   <div class="columns is-mobile my-4 mx-0">
     <MoleculesMinusButton :click-function="prevDate" />
-    <button
-      class="column is-50px button is-large ml-2"
-      @click="fetchDate"
-    >
-      <span class="icon is-medium">
-        <i class="mdi mdi-calendar-today" />
-      </span>
-    </button>
+    <MoleculesTodayButton :click-function="fetchDate" />
     <input
       v-model="date"
       class="column input is-large mx-2"
@@ -29,10 +22,3 @@ const { fetchDate, prevDate, nextDate } = indexStore
     <MoleculesPlusButton :click-function="nextDate" />
   </div>
 </template>
-
-<style scoped>
-.is-50px {
-  flex: none;
-  width: 50px;
-}
-</style>
